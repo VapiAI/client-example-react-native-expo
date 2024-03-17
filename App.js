0b45995bf44094fd68b95ca015de7ace719db43b
Husam Alrubaye <husamalrubaye@gmail.com>
2024-03-16 20:01:35 -0700

    Working demo of Vapi on React-Native Expo build

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { useVapi } from "./useVapi";
 
 export default function App() {
+  const { toggleCall } = useVapi();
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button
+        title="toggleCall"
+        onPress={() => {
+          toggleCall();
+          console.log("toggling call");
+        }}
+      />
     </View>
   );
 }
@@ -13,8 +23,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ vapi.sdk.ts
@@ -0,0 +1,5 @@
+import Vapi from "@vapi-ai/react-native";
+
+const vapi = new Vapi(process.env.api_key || "");
+
+export default vapi;
