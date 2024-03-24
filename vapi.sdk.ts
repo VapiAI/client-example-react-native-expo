a734d5a9166d956a82f34e2f0e3030656f12c7f4
Husam Alrubaye <husamalrubaye@gmail.com>
2024-03-23 18:24:57 -0700

    disable and Remove API key

--- vapi.sdk.ts
+++ vapi.sdk.ts
@@ -1,7 +1,5 @@
 import Vapi from "@vapi-ai/react-native";
 
-const vapi = new Vapi(
-  process.env.api_key || "812437de-42ce-4bca-a9e7-e62cd170e882"
-);
+const vapi = new Vapi(process.env.api_key);
 
 export default vapi;
