2c6f437982776291d1551119da578684ea1e7cde
Husam Alrubaye <husamalrubaye@gmail.com>
2024-03-23 18:18:07 -0700

    Updated packages/dependencies versions

--- vapi.sdk.ts
+++ vapi.sdk.ts
@@ -1,5 +1,7 @@
 import Vapi from "@vapi-ai/react-native";
 
-const vapi = new Vapi(process.env.api_key || "");
+const vapi = new Vapi(
+  process.env.api_key || "812437de-42ce-4bca-a9e7-e62cd170e882"
+);
 
 export default vapi;
